6df8b1033c5918948d01307d92e4a924f063ec02
SHair <sonhai.it.2048@gmail.com>
2025-12-10 11:06:36 +0700

    Create a basic UI for the Category page

--- app/api/categories/route.ts
+++
@@ -1,9 +0,0 @@
-import { NODE_URL } from "@/env.config";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const res = await fetch(`${NODE_URL}/api/categories`);
-  const data = await res.json();
-
-  return NextResponse.json(data);
-}
