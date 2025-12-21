5d816d8caab8fcc6e908a5dbcba00c617b83c813
SHair <sonhai.it.2048@gmail.com>
2025-12-21 20:27:48 +0700

    Create Login and Register UI

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,3 +1,66 @@
-export default function SignUpPage() {
-  return <div>Sign Up Page</div>;
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useRouter } from "next/navigation";
+
+export default function SignInPage() {
+  const router = useRouter();
+
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>Đăng nhập</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" placeholder="m@example.com" required />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Mật khẩu</Label>
+                <a
+                  href="#"
+                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                >
+                  Quên mật khẩu?
+                </a>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <Button type="submit" className="w-full cursor-pointer">
+          Đăng nhập
+        </Button>
+        <Button variant="outline" className="w-full cursor-pointer">
+          Đăng nhập với Google
+        </Button>
+        <CardAction className="flex w-full items-center justify-center">
+          <span className="text-sm">Chưa có tài khoản?</span>
+          <Button
+            variant="link"
+            className="cursor-pointer"
+            onClick={() => router.push("/sign-up")}
+          >
+            Đăng ký
+          </Button>
+        </CardAction>
+      </CardFooter>
+    </Card>
+  );
 }
