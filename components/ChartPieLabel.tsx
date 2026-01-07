0ebd1365a92f59b2abe84e9c4971adaa925bce39
SHair <sonhai.it.2048@gmail.com>
2026-01-07 23:57:38 +0700

    Displayed dashboard stats in dashboard page

--- components/ChartPieLabel.tsx
+++ components/ChartPieLabel.tsx
@@ -1,16 +1,7 @@
 "use client";
 
-import { TrendingUp } from "lucide-react";
 import { Pie, PieChart } from "recharts";
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import {
   ChartContainer,
   ChartTooltip,
---
+++ components/DashboardStats.tsx
@@ -0,0 +1,42 @@
+interface DashboardStatsProps {
+  crawledArticlesNumber: number;
+  newUsersTodayNumber: number;
+  summarizedArticlesNumber: number;
+}
+
+export default async function DashboardStats({
+  crawledArticlesNumber,
+  newUsersTodayNumber,
+  summarizedArticlesNumber,
+}: DashboardStatsProps) {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
+      <div className="rounded-xl border bg-card text-card-foreground p-6">
+        <div className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <p className="text-sm font-medium text-muted-foreground">Bài báo mới hôm nay</p>
+          <span className="text-green-500 text-xs font-bold">+12%</span>
+        </div>
+        <div className="text-2xl font-bold">+{crawledArticlesNumber}</div>
+        <p className="text-xs text-muted-foreground mt-1">So với hôm qua</p>
+      </div>
+
+      <div className="rounded-xl border bg-card text-card-foreground p-6">
+        <div className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <p className="text-sm font-medium text-muted-foreground">Người dùng mới</p>
+        </div>
+        <div className="text-2xl font-bold">+{newUsersTodayNumber}</div>
+        <p className="text-xs text-muted-foreground mt-1">Hoạt động tích cực</p>
+      </div>
+
+      <div className="rounded-xl border bg-card text-card-foreground p-6">
+        <div className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <p className="text-sm font-medium text-muted-foreground">
+            Bài báo đã tóm tắt hôm nay
+          </p>
+        </div>
+        <div className="text-2xl font-bold">+{summarizedArticlesNumber}</div>
+        <p className="text-xs text-muted-foreground mt-1">Xử lý bởi AI</p>
+      </div>
+    </div>
+  );
+}
