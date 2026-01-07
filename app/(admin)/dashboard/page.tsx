152746ce19278697e4f00c7d1459451308f07ae5
SHair <sonhai.it.2048@gmail.com>
2026-01-07 23:57:38 +0700

    Displayed dashboard stats in dashboard page

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,28 +1,79 @@
 import { ChartBarHorizontal } from "@/components/ChartBarHorizontal";
 import { ChartPieLabel } from "@/components/ChartPieLabel";
+import DashboardStats from "@/components/DashboardStats";
+import envConfig from "@/env.config";
+import { cookies } from "next/headers";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value;
+  let crawledArticlesNumber = 0;
+  let newUsersTodayNumber = 0;
+  let summarizedArticlesNumber = 0;
+
+  if (token) {
+    try {
+      const [crawledArticlesRes, newUsersTodayRes, summarizedArticlesRes] =
+        await Promise.all([
+          fetch(`${envConfig.NEXT_PUBLIC_API_ENDPOINT}/articles/stats/crawled-articles`, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+            cache: "no-store",
+          }),
+
+          fetch(`${envConfig.NEXT_PUBLIC_API_ENDPOINT}/users/stats/new-users`, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+            cache: "no-store",
+          }),
+
+          fetch(
+            `${envConfig.NEXT_PUBLIC_API_ENDPOINT}/articles/stats/summarized-articles`,
+            {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+              cache: "no-store",
+            }
+          ),
+        ]);
+
+      if (crawledArticlesRes.ok) {
+        const payload = await crawledArticlesRes.json();
+        crawledArticlesNumber = payload.data.todayTotal;
+      }
+      if (newUsersTodayRes.ok) {
+        const payload = await newUsersTodayRes.json();
+        newUsersTodayNumber = payload.data.newUsersToday;
+      }
+
+      if (summarizedArticlesRes.ok) {
+        const payload = await summarizedArticlesRes.json();
+        summarizedArticlesNumber = payload.data.todayCount;
+      }
+    } catch (error) {
+      console.error("Error checking bookmark status: ", error);
+    }
+  }
+
   return (
     <div className="space-y-4">
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-2">
-        <div className="bg-primary-foreground p-4 rounded-lg">
-          {/* <AppBarChart /> */}
-          Text
-        </div>
-        <div className="bg-primary-foreground p-4 rounded-lg">
-          {/* <CardList title="Latest Transactions" /> */}
-          Text
-        </div>
-        <div className="bg-primary-foreground p-4 rounded-lg">
-          {/* <AppPieChart /> */}
-          Text
-        </div>
-      </div>
+      <DashboardStats
+        {...{ crawledArticlesNumber, newUsersTodayNumber, summarizedArticlesNumber }}
+      />
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-2">
-        <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <div className="bg-primary-foreground p-4 rounded-sm lg:col-span-2 xl:col-span-1 2xl:col-span-2">
           <ChartBarHorizontal />
         </div>
-        <div className="bg-primary-foreground p-4 rounded-lg">
+        <div className="bg-primary-foreground p-4 rounded-sm">
           <ChartPieLabel />
         </div>
       </div>
